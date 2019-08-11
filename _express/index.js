bf9ad53d4a9e39dd537467e7f377f1949f3e1c8c
Kevin Krato <kratok08@gmail.com>
2019-08-10 20:20:41 -0600

    Validate and post new people

--- _express/index.js
+++ _express/index.js
@@ -1,11 +1,40 @@
 const express = require("express");
+const Joi = require("@hapi/joi");
 const app = express();
 
 app.use(express.json());
 
+const people = [
+  { id: 1, name: "Steven", age: 15 },
+  { id: 2, name: "Richard", age: 53 },
+  { id: 3, name: "Sammy", age: 21 }
+];
+
 app.get("/", (req, res) => {
-  res.send("Welcome friendo.");
+  res.send(people);
 });
+
+app.post("/api/addperson/", (req, res) => {
+  const personSchema = {
+    name: Joi.string()
+      .min(1)
+      .required(),
+    age: Joi.number()
+      .min(1)
+      .required()
+  };
+  const { error } = Joi.validate(req.body, personSchema);
+  if (error) return res.status(400).send(error.details[0].message);
+  const newPerson = {
+    id: people.length + 1,
+    name: req.body.name,
+    age: req.body.age
+  };
+  people.push(newPerson);
+  res.send(people);
+});
+
+app.get("/api/findUser:id", (req, res) => {});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`Successfully connected to port ${PORT}...`));
